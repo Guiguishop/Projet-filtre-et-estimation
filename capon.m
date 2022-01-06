cfb6731ceda9fac7974196ae6886b9f8c365e670
Guiguishop <71508215+Guiguishop@users.noreply.github.com>
2022-01-06 17:11:40 +0100

    capon marche

--- capon.m
+++ capon.m
@@ -1,25 +1,18 @@
-function [P,returned] = capon(signal, f, fech,mode)
-N=length(signal);
-if (length(signal)~= length(f))
-    disp("probleme de dimension");
-end
-a= zeros(N,length(f));
-for j=1:length(f )
-    for k=1:N
-        a(j,k)=exp(-i*2*pi*(k-1)*(f(j)/fech));
+function [P] = capon(signal, f, fech)
+    N=length(signal);
+    if (length(signal)~= length(f))
+        disp("probleme de dimension");
     end
+    
+    a=zeros(1,N);
+    P=zeros(1,length(f));
+    Rx=signal'*signal;
+    invRx = 1\Rx;
+    for j=1:length(f)
+        for k=1:N
+            a(k)= exp(-1i*2*pi*(k-1)*(f(j)/fech)); %Steering vecteur
+        end
+        P(j) = 1./ a*invRx*a';
+    end     
 end
-Rx=signal'*signal;
-P=zeros(N,length(f ));
-invRx=1\Rx;
-if (mode ==1)
- returned=invRx
-end
-if (mode==0)
-for k=1:length(f)
-
-P(:,k)=1/(a*invRx*a');
-end
-end
-
-end 
+ 
--- filtre_estimation_cyril.m
+++ filtre_estimation_cyril.m
@@ -7,7 +7,7 @@ close all
 Nombre_point=1000;
 var_bruit=3;
 fech=10000;
-fo=1000;
+fo=4500;
 Te=1/fech;
 abscisse=0:1:Nombre_point-1;
 bruit = randn(1,Nombre_point)*var_bruit;
@@ -34,6 +34,7 @@ title("signal sinusoidale");
 
 %Fft : 
 Nfft = Nombre_point;
+signalnonpadde = signal;
 % permet d'ajouter le padding nÃ©cessaire pour avoir une puissance de 2 
 while ((log2(Nfft)-floor(log2(Nfft))) ~= 0)
     Nfft=Nfft+1;
@@ -116,7 +117,10 @@ title("periodogramme Welch new version");
 % imagesc(temps,frequence,spectro);
 
 %% méthode capon 
-[P invRx]= capon(signal, abscissef, fech,0);
+[P]= capon(signal, abscissef, fech);
+figure()
+plot(abscissef,abs(P)),title("Puissance")
+
 
 %% Méthode des trapezes
 fmin = 100;
@@ -124,7 +128,6 @@ fmax = 300;
 integrale2 = method_trapeze(periodogrammedaniel,fmin,fmax,fech);
 integrale3 = method_trapeze(periodogrammemoyenne,fmin,fmax,fech);
 integrale4 = method_trapeze(periodogrammewelch2,fmin,fmax,fech);
-disp(["IntégraleWelch entre" fmin "et" fmax "vaut" integrale1])
 disp(["IntégraleDaniel entre" fmin "et" fmax "vaut" integrale2])
 disp(["IntégraleMoyenne entre" fmin "et" fmax "vaut" integrale3])
 disp(["IntégraleWelch2 entre" fmin "et" fmax "vaut" integrale4])
